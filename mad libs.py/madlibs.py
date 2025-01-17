6610eabf067151f45ec548c49a15e88e3ad567ee
agalfaro1 <agalfaro1@cps.edu>
2025-01-17 00:06:22 -0600

    Add files via upload

--- mad libs.py/madlibs.py
+++ mad libs.py/madlibs.py
@@ -1,1 +1,35 @@
+# Input prompts
+adjective = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+plural_noun = input("Enter a plural noun: ")
+game = input("Enter a name of a game: ")
+plural_noun2 = input("Enter a plural noun: ")
+ing_verb = input("Enter a verb ending in -ing: ")
+ing_verb2 = input("Enter a verb ending in -ing: ")
+plural_noun3 = input("Enter a plural noun: ")
+ing_verb3 = input("Enter a verb ending in -ing: ")
+noun3 = input("Enter a noun: ")
+plant = input("Enter the name of a plant: ")
+body_part = input("Enter the name of a body part: ")
+place = input("Enter a name of a place: ")
+ing_verb4 = input("Enter a verb ending in -ing: ")
+adjective3 = input("Enter an adjective: ")
+number = input("Enter a number: ")
+plural_noun4 = input("Enter a plural noun: ")
 
+# Story template
+story = ("I still miss the " + adjective + " toilet moments I had with my " +
+         adjective2 + " friend. Usually those moments happen near a " + noun +
+         " or up in my " + noun2 + ". However, this time I took my " + plural_noun +
+         " to play " + game + " but when they don't want to play we go see " + plural_noun2 +
+         ". I like to spend my time " + ing_verb + " or " + ing_verb2 + ". When I am all alone, I enjoy eating multiple " +
+         plural_noun3 + " a day. That only happens when I am in a good mood. When I feel upset, I go " + ing_verb3 +
+         ". Last year, I went to ride a " + noun3 + " and got diarrhea. " + plant + " helped me with " + body_part +
+         " pain. This year I want to explore " + place + ", and I will no longer be " + ing_verb4 +
+         ". Maybe this time I will be more careful and not so " + adjective3 + ", also because I drink too much chocolate milk " +
+         number + " hours every day until I am once again stuck on the toilet. I used to think " + plural_noun4 + " were lactose intolerant.")
+
+# Main output
+print(story)
